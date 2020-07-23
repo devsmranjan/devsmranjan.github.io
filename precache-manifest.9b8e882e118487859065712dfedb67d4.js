241ca9a0f4343b5b11f64c827dfe310b292a3f72
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2020-07-23 10:17:48 +0530

    Updates

---
+++ precache-manifest.9b8e882e118487859065712dfedb67d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebb1f262ee248b20614c15720c4e365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc370866e4aed4b757e",
+    "url": "/static/css/main.fd014b21.chunk.css"
+  },
+  {
+    "revision": "e3b15c07dbedf1be4870",
+    "url": "/static/js/2.300c4b9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.300c4b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc370866e4aed4b757e",
+    "url": "/static/js/main.7e59f503.chunk.js"
+  },
+  {
+    "revision": "bd170c4b7f7cc1c96189",
+    "url": "/static/js/runtime-main.5c7396b5.js"
+  }
+]);
